7a5c458dce0fcc452d39bb1a71794dcf88ab5ac1
Aditya Sharma <adityadgr29@gmail.com>
2026-07-10 07:40:00 +0530

    auditlog

---
+++ C/audi.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    ssize_t numRead;
+    
+
+    if (argc < 2)
+    {
+        printf("Usage:\n");
+        printf("./auditlog --add \"message\"\n");
+        printf("./auditlog --view\n");
+        exit(1);
+    }
+
+    if (strcmp(argv[1], "--add") == 0)
+    {
+        if (argc!= 3)
+        {
+            printf("Usage: ./auditlog --add \"message\"\n");
+            exit(1);
+        }
+
+        fd = open("auditlog.txt",
+                  O_CREAT | O_WRONLY | O_APPEND |
+                S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+        if (fd == -1)
+        {
+            perror("open");
+            exit(1);
+        }
+
+        write(fd, argv[2], strlen(argv[2]));
+        write(fd, "\n", 1);
+        close(fd);
+    }
+    else if (strcmp(argv[1], "--view") == 0)
+    {
+        if (argc != 2)
+        {
+            printf("Usage: ./auditlog --view\n");
+            exit(1);
+        }
+
+        fd = open("auditlog.txt", O_RDONLY);
+
+        if (fd == -1)
+        {
+            perror("open");
+            exit(1);
+        }
+
+
+        char ch;
+        int line=1;
+        printf("%d:",line);
+
+        while(read(fd,&ch,1)>0){
+            printf("%c",ch);
+            if(ch=='\n'){    
+                 line++;
+                printf("%d: ", line);
+            }
+        }
+        printf("\n");
+
+        close(fd);
+    }
+    else
+    {
+        printf("Unknown command.\n");
+        printf("./auditlog --add \"message\"\n");
+        printf("./auditlog --view\n");
+    }
+
+    return 0;
+}
