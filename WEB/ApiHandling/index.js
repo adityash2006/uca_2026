1108b789515ccae3816254cae7584cb9d4dade11
unknown <adityadgr29@gmail.com>
2026-07-25 10:42:04 +0530

    api results on ui

--- WEB/ApiHandling/index.js
+++ WEB/ApiHandling/index.js
@@ -4,14 +4,22 @@ web1.postMessage("");
 web1.onmessage= function(event){
    console.log("data from worker file")
     if(event.data==null){
+        document.getElementById("users").innerHTML=`<div> No users found </div>`
         console.log("No users from the api received");
         return;
     }
    for(const obj of event.data){
+
     console.log(`${obj.name} - ${obj.status==true?"Active":"Inactive"}`);
      // consoling status as active for age greater than 34
    }
 
+   document.getElementById("users").innerHTML=`
+   <div>
+                ${event.data.map((obj)=>`<p>${obj.name} - ${obj.status==true?"Active":"Inactive"}</p>`).join(" ")}
+
+                </div>
+   `
    console.log("out of the worker");
 
 }
--- WEB/ApiHandling/worker.js
+++ WEB/ApiHandling/worker.js
@@ -2,9 +2,18 @@
 
 self.onmessage=async function (){
     console.log("Hi from worker file ")
-    const a= await fetch("https://dummyjson.com/users");
-    const b= await a.json();
-    const ans= b.users.map((obj)=>{return {"name":obj.firstName,"status":obj.age>34}})
-   
-   self.postMessage(ans);
+    let ans=null;
+    try {
+        const a= await fetch("https://dummyjson.com/users");
+        const b= await a.json();
+        ans= b.users.map((obj)=>{return {"name":obj.firstName,"status":obj.age>34}})
+
+    } catch (error) {
+        console.log("error",error)
+    }finally{
+ self.postMessage(ans);
+    
+    }
+       
+    
 }
